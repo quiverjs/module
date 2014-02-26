3f204516ab5a3a5e83f5ece237ea287c934eff3f
Soares Chen <soares.chen@quiverjs.org>
2014-02-26 23:04:56 +0800

    Revamped quiver-module and make it into evil synchronous mode.

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@
 var async = require('async')
 var should = require('should')
 var testModule = require('../test-module')
-var moduleLib = require('../lib/export')
+var moduleLib = require('../lib/module')
 
 var expectedComponents = [
   'test component 0',
@@ -27,19 +27,14 @@ var componentListToTable = function(components) {
 }
 
 describe('integrated module test', function() {
-  it('should load all modules', function(callback) {
-    moduleLib.loadComponentsFromQuiverModule(testModule.quiverModule, 
-      function(err, quiverComponents) {
-        if(err) console.log(err)
-        if(err) return callback(err)
+  it('should load all modules', function() {
+    var quiverComponents = moduleLib.loadComponentsFromQuiverModule(
+      testModule.quiverModule)
 
-        var componentTable = componentListToTable(quiverComponents)
-
-        expectedComponents.forEach(function(componentName) {
-          should.exists(componentTable[componentName])
-        })
-
-        callback()
-      })
+    should.equal(quiverComponents.length, expectedComponents.length)
+    
+    quiverComponents.forEach(function(component) {
+      should.notEqual(expectedComponents.indexOf(component.name), -1)
+    })
   })
 })
